331abd06953abd4fb9c606aabe288e231b20178c
Bruno Costa <brunovasquescosta@gmail.com>
2024-01-27 19:53:57 +0000

    Places validator in docker container to ensure valid run, changes validator into classes

---
+++ validator/webserver.py
@@ -0,0 +1,48 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import json
+from miappe_validator import Miappe_validator as mv
+
+hostName = "0.0.0.0"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print(self)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+    def get_body(self):
+        content_len = int(self.headers.get('Content-Length'))
+        return self.rfile.read(content_len)
+
+    def do_POST(self):
+        body = json.loads(self.get_body())
+        logs = mv(body['file']).run_miappe_validator()
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>OntoBrapi Validator</title></head>", "utf-8"))
+        self.wfile.write(bytes("<body><p>", "utf-8"))
+        for logLine in logs:
+            self.wfile.write(bytes(logLine, "utf-8"))
+            self.wfile.write(bytes("<br>", "utf-8"))
+        self.wfile.write(bytes("</p></body></html>", "utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
