e728d17415e8bcb7f6c274a7422f5f661e2382d6
Bruno Costa <netbofia@hotmail.com>
2024-03-15 18:02:10 +0000

    Adds the timer to testing of validator

--- validator/test_miappe_validator.py
+++ validator/test_miappe_validator.py
@@ -1,4 +1,14 @@
+#!/usr/bin/env python3
 import sys
+import time
+t1 = time.perf_counter(), time.process_time()
 from miappe_validator import Miappe_validator as mv
 input_file=sys.argv[1]
 mv(input_file).run_miappe_validator()
+
+t2 = time.perf_counter(), time.process_time()
+#print(f"{function.__name__}()")
+print(f" Real time: {t2[0] - t1[0]:.2f} seconds")
+print(f" CPU time: {t2[1] - t1[1]:.2f} seconds")
+print()
+
