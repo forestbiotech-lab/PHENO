0c9e4e5390375f5ca9e3f5beaa90e9be00c8a425
Bruno Costa <brunovasquescosta@gmail.com>
2024-03-18 16:51:40 +0000

    Updates the setup script to run the repo setup

--- setup.sh
+++ setup.sh
@@ -51,27 +51,8 @@ sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 #Login to USERNAME and finish install
 sudo su ${USERNAME}
 
-## FIX THIS PART must be ran by the new user 
-cd
-mkdir git
-cd git
-git clone https://github.com/forestbiotech-lab/ontoBrAPI.git
-cd ontoBrAPI
-git submodule init
-git submodule set-url ontoBrAPI-node-docker  https://github.com/forestbiotech-lab/ontoBrAPI-node-docker.git
-git submodule set-url ontobrapi-admin  https://github.com/forestbiotech-lab/ontobrapi-admin.git
-git submodule set-url ontobrapi-brapi  https://github.com/forestbiotech-lab/ontobrapi-brapi.git
-git submodule update
-cd ontoBrAPI-node-docker
-git checkout master
-cd ../ontobrapi-admin
-git checkout main
-cd ../ontobrapi-brapi
-git checkout master
-cd ..
-
-docker-compose up -d 
-
+wget https://raw.githubusercontent.com/forestbiotech-lab/ontoBrAPI/master/reposetup.sh -O reposetup.sh
+sudo --user ${USERNAME} bash reposetup.sh
 echo "DONE - Finished install"
 
 
